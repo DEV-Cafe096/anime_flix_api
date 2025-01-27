5a3df68e0f8630da1e3e0eca5f96af4c1c9a7868
Michel_Correa <devcafe096@gmail.com>
2025-01-27 06:43:48 -0300

    adicionada ordenacao, genero e lingua dos animes

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,29 @@
-import express from "express";
-import { PrismaClient } from "@prisma/client";
-
+import express from 'express';
+import { PrismaClient } from '@prisma/client';
 
 const port = 3000;
 const app = express();
 const prisma = new PrismaClient();
 
-app.get('/animes', async (req, res) => {
-    const animes = await prisma.anime.findMany();
-    res.json(animes);
-})
+app.get('/animes', async (_, res) => {
+	try {
+		const animes = await prisma.anime.findMany({
+            orderBy: {
+                title: 'asc'
+            },
+            include: {
+                genres: true,
+                languages: true
+                
+            }
+        });
+		res.json(animes);
+	} catch (error) {
+		console.error(error);
+		res.status(500).json({ error: 'Erro ao buscar animes' });
+	}
+});
 
 app.listen(port, () => {
-    console.log(`Servidor executando na porta ${port}`);
+	console.log(`Servidor executando na porta ${port}`);
 });
