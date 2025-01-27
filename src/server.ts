3fe26b8c82ee28ec59589d5e9cd190a9bfed66ef
Michel_Correa <devcafe096@gmail.com>
2025-01-27 07:50:21 -0300

    adicionado mensagem de erro na criacao do anime no db

--- src/server.ts
+++ src/server.ts
@@ -1,26 +1,50 @@
 import express from 'express';
 import { PrismaClient } from '@prisma/client';
 
+
 const port = 3000;
 const app = express();
 const prisma = new PrismaClient();
 
+// Middleware para parsing do corpo da requisição como JSON
+app.use(express.json());
+
 app.get('/animes', async (_, res) => {
 	try {
 		const animes = await prisma.anime.findMany({
-            orderBy: {
-                title: 'asc'
-            },
-            include: {
-                genres: true,
-                languages: true
-                
-            }
-        });
+			orderBy: {
+				title: 'asc',
+			},
+			include: {
+				genres: true,
+				languages: true,
+			},
+		});
 		res.json(animes);
 	} catch (error) {
 		console.error(error);
 		res.status(500).json({ error: 'Erro ao buscar animes' });
+	}
+});
+
+app.post('/animes', async (req, res) => {
+	const { title, genre_id, language_id, description, release_date } = req.body;
+
+	try {
+		await prisma.anime.create({
+			data: {
+				title: title,
+				genre_id: genre_id,
+				language_id: language_id,
+				description: description,
+				release_date: new Date(release_date)
+			},
+		});
+		// Enviar uma resposta de sucesso
+		res.status(201).send({ message: 'Anime criado com sucesso!' });
+	} catch (error) {
+		console.error(error);
+		res.status(500).send({ message: 'Erro ao criar anime' });
 	}
 });
 
